6c30e7c5c0fc2f81dd4fdef83b2aa459d08b16b1
DukeNova <noahlepago@gmail.com>
2016-10-25 14:12:57 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+function setup() {
+  createCanvas(800,600)
+  background(91,222,225)
+}
+
+function draw() {
+    fill(255);
+  ellipse(120,600,340,1000);
+    fill(0);
+  ellipse(412,362,50,60);
+    fill(0);
+  ellipse(412,340,50,50);
+    fill(255);
+  ellipse(404,332,10,10);
+    fill(0);
+  ellipse(404,332,6,6);
+    fill(255);
+  ellipse(420,332,10,10);
+    fill(0);
+  ellipse(420,332,6,6);
+    fill(255,126,0)
+  triangle(403, 340, 413, 360, 423, 340);
+    fill(255)
+  ellipse(320,800,340,1000);
+    fill(255)
+  ellipse(780,800,340,1000);
+    fill(255)
+  ellipse(552,700,340,1000);
+  
+}
